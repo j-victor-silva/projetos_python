324551d54c482e8a6bd955a53ce3846453a19675
João Victor da Silva <jvictor200212@gmail.com>
2022-04-25 09:43:15 -0300

    Atualização

--- Automatizar Excel/funcoes.py
+++ Automatizar Excel/funcoes.py
@@ -1,33 +1,34 @@
 import pyautogui as py
 import pyperclip as pyper
-import time
+import time as t
 
 
 def copiar_conhecimento(dict, index):
     """Função para copiar o campo de conhecimento"""
     pyper.copy(dict[index])
-    time.sleep(1)
     py.press('enter')
     with py.hold('ctrl'):
         py.press('v')
         py.press('enter')
+    t.sleep(0.5)
     py.press('enter')
+    t.sleep(0.1)
     py.press('up')
-    
-    
+
+
 def copiar_objetivo(dict, key, chave=''):
     """Função para copiar o objetivo(ID) e sua descrição"""
     for k in dict.keys():
         if not k == key:
             continue
-        
+
         chave = k
-    
+
     pyper.copy(chave)
-    time.sleep(1)
+    t.sleep(1)
     py.press('enter')
     py.hotkey('ctrl', 'v')
-    
+
     pyper.copy(f'\n{dict[key]}')
     with py.hold('ctrl'):
         py.press('enter')
@@ -35,36 +36,33 @@ def copiar_objetivo(dict, key, chave=''):
         py.press('enter')
         py.press('enter')
     py.press('enter')
-    py.press('up')    
+    py.press('up')
+
 
 def colar_asterisco(quant):
     """Função para colocar asteríscos que serão utilizados para 
        digitar o planejamento"""
     quant1 = 0
-    time.sleep(1)
+    t.sleep(1)
     while quant1 != quant:
         py.hotkey('ctrl', 'enter')
         py.write('*')
         quant1 += 1
-    
+
 
 def data(dia, mes, aula, quant):
     """Funcao para colocar o dia/mes e a aula correspondente"""
-    time.sleep(1)
+    t.sleep(1)
     for i in range(quant):
         if not dia >= 10:
             py.write(f'0{dia}/0{mes}')
-        
+
         else:
             py.write(f'{dia}/0{mes}')
-            
+
         py.hotkey('ctrl', 'enter')
 
         py.write(f'Aula {aula}')
         dia += 1
         aula += 1
         py.press('enter')
-        
-
-        
-
--- Automatizar Excel/main.py
+++ Automatizar Excel/main.py
@@ -6,7 +6,7 @@ O programa vai basicamente copiar coisas que são muito usadas para as células 
 import funcoes as func
 import pyperclip as pyper
 import pyautogui as py
-import time
+import time as t
 
 
 conhecimento = {
@@ -19,7 +19,7 @@ conhecimento = {
 }
 
 objetivos = {
-    'EI03EF01CRU': 'Expressar ideias, desejos e sentimentos sobre suas vivências, por meio da linguagem oral e escrita',
+    'EI03EF01CRU': 'Expressar ideias, desejos e se     tntos sobre suas vivências, por meio da linguagem oral e escrita',
     'EI03CG05CRU': 'Coordenar suas habilidades manuais no atendimento adequadro a seus interesses e necessidades',
     'EI03TS02CRU': 'Expressar-se livremente por meio de desenhos, pintura e colagem',
     'EI03ET07CRU': 'Relacionar números a suas respectivas quantidades',
@@ -30,11 +30,13 @@ objetivos = {
     'EI03ET03CRU': 'Identificar e selecionar fontes de informações, para responder a questões sobre a natureza',
     'EI03CG05CRU': 'Coordenar suas habilidades manuais no atendimento',
     'EI03ET01CRU': 'Estabelecer relações de comparação entre objetos, observando suas propriedades',
-    'EI03CG01CRU': 'Criar com o corpo formas diversificadas de expressão de sentimentos, sensações e emoções, tanto nas situações do cotidiano quanto em brincadeiras, dança, teatro, etc.',
+    'EI03CG01CRU': 'Criar com o corpo formas diversificadas de expressão de se     tntos, sensações e emoções, tanto nas situações do cotidiano quanto em brincadeiras, dança, teatro, etc.',
     'EI03ET05': 'Classificar e reconhecer objetos e figuras de acordo com suas semelhanças e diferenças',
     'EI03E003CRU': 'Ampliar as relações interpessoais, desenvolvendo atitudes de participação, cooperação e solidariedade',
     'EI03EF02CRU': 'Inventar brincadeiras, cantadas, poemas e canções, criando rimas',
-    'EI03ET04CRU': 'Registrar observações, manipulações e medidas, usando múltiplas linguagens (desenho, registro por números ou escrita espontânea)'
+    'EI03ET04CRU': 'Registrar observações, manipulações e medidas, usando múltiplas linguagens (desenho, registro por números ou escrita espontânea)',
+    'EI02CG05CRU': 'Coordenar suas habilidades manuais no atendimento adequado a seus interesses e necessidades em situações diversas',
+    'EI03EF07CRU': 'Levantar hipóteses sobre gêneros textuais veiculados em portadores conhecidos recorrendo a estratégias de observação',
 }
 
 
@@ -56,8 +58,8 @@ while True:
 
         escolha = input(
             f'Aqui está a lista de conhecimentos:\n\n{conhecimento_formatado}\n->')
-        time.sleep(0.5)
         py.hotkey('alt', 'tab')
+        t.sleep(0.1)
         func.copiar_conhecimento(conhecimento, int(escolha))
 
     elif escolha == '2':
@@ -67,23 +69,27 @@ while True:
 
         escolha = input(
             f'Aqui está a lista de objetivos:\n\n{objetivos_formatados}\n->')
-        time.sleep(0.5)
         py.hotkey('alt', 'tab')
+        t.sleep(0.1)
         func.copiar_objetivo(objetivos, escolha)
 
     elif escolha == '3':
         escolha = input(
             'Digite a quandidade de asteríscos que deseja colocar: ')
-        time.sleep(0.5)
         py.hotkey('alt', 'tab')
+        t.sleep(0.1)
         func.colar_asterisco(int(escolha))
 
-    else:
+    elif escolha == '4':
         dia = input('Digite o dia: ')
         mes = input('Digite o mês: ')
         aula = input('Digite a aula: ')
         quantidade = input('Digite a quantidade de repetições: ')
-        time.sleep(0.5)
         py.hotkey('alt', 'tab')
+        t.sleep(0.1)
         func.data(int(dia), int(mes), int(aula), int(quantidade))
-        
+
+    else:
+        print('Você precisa digitar uma opção válida.')
+        t.sleep(1)
+        continue
