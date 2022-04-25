ecfabaa2a9904b462964cef010428b3a708ea5f9
João Victor da Silva <jvictor200212@gmail.com>
2022-04-25 09:44:41 -0300

    Atualização

---
+++ Youtube Video Downloader/main.py
@@ -0,0 +1,9 @@
+'''Youtube Video Downloader & Converter
+
+O programa irá receber um link do Youtube, fazer o download do vídeo e em
+seguida irá ser apresentado opções para o usuário como:
+-A pasta que será salvado o vídeo;
+-Se deseja converter para outra extensão;
+-Se deseja converter para MP3.
+'''
+import classe
