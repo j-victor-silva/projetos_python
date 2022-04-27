14ab5732a6283dc113366ceb9b362ad2befec187
João Victor da Silva <jvictor200212@gmail.com>
2022-04-27 13:30:40 -0300

    Corrigido problema em diretórios

--- Organizar Arquivos/class.py
+++ Organizar Arquivos/class.py
@@ -4,18 +4,12 @@ from sys import platform
 
 
 """Extensões para detectar para onde os arquivos irão"""
-ext_texto = ('.pdf', '.docx', '.txt', '.json')
-ext_program = ('.exe', '.msi', '.run')
+ext_texto = ('.pdf', '.docx', '.txt', '.json', '.pptx', '.xlsx')
+ext_program = ('.exe', '.msi', '.run', '.apk', '.reg')
 ext_compress = ('.rar', '.zip', '.7z', '.deb')
-
-
-"""Extensões para detectar para onde os arquivos irão"""
-ext_texto = ('.pdf', '.docx', '.txt')
-ext_program = ('.exe', '.msi')
-ext_compress = ('.rar', '.zip', '.7z')
 ext_music = ('.mp3')
 ext_video = ('.mp4', '.mkv')
-ext_imagem = ('.jpeg', '.jpg', '.png')
+ext_imagem = ('.jpeg', '.jpg', '.png', '.gif', '.jfif', '.webp', '.GIF')
 
 """Novos diretórios, coloque aqui para onde cada arquivo vai
    seguindo a ordem:
@@ -26,7 +20,9 @@ ext_imagem = ('.jpeg', '.jpg', '.png')
    5-Videos
    6-Imagens
 """
-def letter_disk(user: str, l: str='C'):
+
+
+def letter_disk(user: str, l: str = 'C'):
     global dir_original, new_dirs
     '''Função para definir o disco que será salvo os arquivos'''
     if platform == 'linux':
@@ -37,27 +33,20 @@ def letter_disk(user: str, l: str='C'):
                     5: f'/home/{user}/Vídeos', 6: f'/home/{user}/Imagens'}
 
     else:
-        dir_original = f'{l}:\\Downloads'
+        dir_original = fr'{l}:\Downloads'
 
-        new_dirs = {1: f'{l}:\\Arquivos\\Documents', 2: f'{l}:\\Arquivos\\Programs',
-                    3: f'{l}:\\Arquivos\\Compressed', 4: f'{l}:\\Arquivos\\Music',
-                    5: f'{l}:\\Arquivos\\Video', 6: f'{l}:\\Arquivos\\Imagens',}
-dir_original = ''
-new_dirs = dict({})
-def letter_disk(l='C'):
-    '''Função para definir o disco que será salvo os arquivos'''
-    new_dirs = {1: f'{l}:\\Arquivos\\Documents', 2: f'{l}:\\Arquivos\\Programs',
-                3: f'{l}:\\Arquivos\\Compressed', 4: f'{l}:\\Arquivos\\Music',
-                5: f'{l}:\\Arquivos\\Video', 6: f'{l}:\\Arquivos\\Imagens',}
-    dir_original = f'{l}:\\Downloads'
+        new_dirs = {1: fr'{l}:\Arquivos\Documents', 2: fr'{l}:\Arquivos\Programs',
+                    3: fr'{l}:\Arquivos\Compressed', 4: fr'{l}:\Arquivos\Music',
+                    5: fr'{l}:\Arquivos\Video', 6: fr'{l}:\Arquivos\Imagens', 
+                    }
 
 
 class Organizador:
     '''Classe que irá fazer todo o trabalho de mover os arquivos'''
     conta = 0
-    
+
     def __init__(self, dir_original: str, new_dir: dict, type_tex=None, type_program=None,
-                 type_compress=None, type_music=None, 
+                 type_compress=None, type_music=None,
                  type_video=None, type_imagem=None) -> None:
         '''Método construtor, aqui os type estão em None por que
            você pode ou não mover alguns arquivos, se você quiser.
@@ -67,7 +56,7 @@ class Organizador:
            Coloque:
            obj_test = Organizador(dir_original, new_dirs, type_tex=ext_texto,
            type_imagem=ext_imagem)'''
-        
+
         self._dir_original = dir_original
         self._new_dir = new_dir
         self.type_tex = type_tex
@@ -76,29 +65,29 @@ class Organizador:
         self.type_music = type_music
         self.type_video = type_video
         self.type_imagem = type_imagem
-        
+
     @property
     def dir_original(self):
         return self._dir_original
-    
+
     @dir_original.setter
     def dir_original(self, valor) -> str:
         if not isinstance(valor, str):
             raise TypeError('O diretório precisa ser em STRING.')
 
         return valor
-    
+
     @property
     def new_dir(self):
         return self._new_dir
-    
+
     @new_dir.setter
     def new_dir(self, valor):
         if not isinstance(valor, tuple):
             raise TypeError('Os novos diretórios precisam estar em uma tupla.')
-        
+
         return self._new_dir
-    
+
     def move_doc(self):
         '''Método que irá mover os documentos'''
         if self.type_tex:
@@ -107,11 +96,12 @@ class Organizador:
                     old_file_path = os.path.join(self.dir_original, file)
                     new_file_path = os.path.join(self.new_dir[1], file)
                     file_wth_ext, ext = os.path.splitext(file)
-                    
+
                     if ext in self.type_tex:
                         self.conta += 1
                         shutil.move(old_file_path, new_file_path)
-        else: pass
+        else:
+            pass
 
     def move_program(self):
         '''Método que irá mover os programas'''
@@ -121,12 +111,13 @@ class Organizador:
                     old_file_path = os.path.join(self.dir_original, file)
                     new_file_path = os.path.join(self.new_dir[2], file)
                     file_wth_ext, ext = os.path.splitext(file)
-                    
+
                     if ext in self.type_program:
                         self.conta += 1
                         shutil.move(old_file_path, new_file_path)
-        else: pass
-        
+        else:
+            pass
+
     def move_compress(self):
         '''Método que irá mover os arquivos zipados'''
         if self.type_compress:
@@ -135,12 +126,13 @@ class Organizador:
                     old_file_path = os.path.join(self.dir_original, file)
                     new_file_path = os.path.join(self.new_dir[3], file)
                     file_wth_ext, ext = os.path.splitext(file)
-                    
+
                     if ext in self.type_compress:
                         self.conta += 1
                         shutil.move(old_file_path, new_file_path)
-        else: pass
-    
+        else:
+            pass
+
     def move_music(self):
         '''Método que irá mover as músicas'''
         if self.type_music:
@@ -149,12 +141,13 @@ class Organizador:
                     old_file_path = os.path.join(self.dir_original, file)
                     new_file_path = os.path.join(self.new_dir[4], file)
                     file_wth_ext, ext = os.path.splitext(file)
-                    
+
                     if ext in self.type_music:
                         self.conta += 1
                         shutil.move(old_file_path, new_file_path)
-        else: pass
-        
+        else:
+            pass
+
     def move_video(self):
         '''Método que irá mover os vídeos'''
         if self.type_video:
@@ -163,12 +156,13 @@ class Organizador:
                     old_file_path = os.path.join(self.dir_original, file)
                     new_file_path = os.path.join(self.new_dir[5], file)
                     file_wth_ext, ext = os.path.splitext(file)
-                    
+
                     if ext in self.type_video:
                         self.conta += 1
                         shutil.move(old_file_path, new_file_path)
-        else: pass
-        
+        else:
+            pass
+
     def move_imagem(self):
         '''Método que irá mover as imagens'''
         if self.type_imagem:
@@ -177,20 +171,21 @@ class Organizador:
                     old_file_path = os.path.join(self.dir_original, file)
                     new_file_path = os.path.join(self.new_dir[6], file)
                     file_wth_ext, ext = os.path.splitext(file)
-                    
+
                     if ext in self.type_imagem:
                         self.conta += 1
                         shutil.move(old_file_path, new_file_path)
-        else: pass
-        
+        else:
+            pass
+
     def create_folders(self):
         '''Método para criar as pastas que serão movidos os arquivos'''
         for folders in self.new_dir.values():
             try:
-                os.mkdir(folders)
+                os.makedirs(f'{folders}')
             except FileExistsError as e:
                 pass
-        
+
     def exec_class(self):
         '''Método que irá executar toda a classe e 
         indicar quantos arquivos serão movidos'''
@@ -204,15 +199,13 @@ class Organizador:
         if not self.conta:
             print('Não há nenhum arquivo para mover.')
             return
-        
+
         print(f'{self.conta} arquivo(s) movido(s) com sucesso!')
-    
 
 
 if __name__ == '__main__':
     letter_disk(input('Digite o user (caso seja linux) ou a letra do disco '
                       'caso seja windows: '))
-    letter_disk(input('Digite a letra do seu disco (C por padrão): '))
-    teste = Organizador(dir_original, new_dirs, ext_texto, ext_program, 
+    teste = Organizador(dir_original, new_dirs, ext_texto, ext_program,
                         ext_compress, ext_music, ext_video, ext_imagem)
     teste.exec_class()
