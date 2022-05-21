bd1f20a38a5f99b14c3d7a273ed73ad7905945fd
João Victor da Silva <jvictor200212@gmail.com>
2022-05-21 20:08:47 -0300

    Adicionada documentação

--- Gerenciador de Banco de Dados/app.py
+++ Gerenciador de Banco de Dados/app.py
@@ -6,16 +6,30 @@ from Login.login_window import *
 
 
 class App:
+    '''Classe que conecta os dois programas
+    
+    A classe App irá conectar a janela de login e caso seja autenticado
+    com sucesso, irá abrir o gerenciador'''
+    
     def __init__(self):
+        # Instância para conectar a tela de login e sua Database
         self.login = LoginWindow('usuarios')
+        # Instância para conectar o gerenciador
         self.gerenciador = app_gerenciador.Gerenciador()
         
         self.login.btnEntrar.clicked.connect(self.entrar)
         
     def abrir(self):
+        '''Método para abrir a tela de login'''
+        
         self.login.show()
 
     def entrar(self):
+        '''Método para abrir o gerenciador
+        
+        Caso seja autenticado o login, irá abrir o gerenciador e fechar
+        a tela de login'''
+        
         self.login.autenticar()
         if self.login.valido:
             self.login.close()
