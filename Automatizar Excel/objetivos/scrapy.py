12ad90ebf5cf6336abfa24c492c48f73966251bf
João Victor da Silva <jvictor200212@gmail.com>
2022-07-04 10:16:14 -0300

    Atualização 04/07 10:16 - Convertido objetivos para json

--- Automatizar Excel/objetivos/scrapy.py
+++ Automatizar Excel/objetivos/scrapy.py
@@ -5,16 +5,30 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 
 
-def sub_especial_characters(string: str):
-    new_string = re.sub(r'[àáã]', 'a', string)
-    new_string = re.sub(r'[ú]', 'u', new_string)
-    new_string = re.sub(r'[óõ]', 'o', new_string)
-    new_string = re.sub(r'[êé]', 'e', new_string)
-    new_string = re.sub(r'[í]', 'i', new_string)
-    new_string = re.sub(r'[ç]', 'c', new_string)
+def sub_especial_characters(older_list: list) -> list:
+    new_list = older_list
+    for i in range(len(older_list)):
+        new_list[i] = re.sub(r'[àáã]', 'a', new_list[i])
+        new_list[i] = re.sub(r'[ú]', 'u', new_list[i])
+        new_list[i] = re.sub(r'[óõ]', 'o', new_list[i])
+        new_list[i] = re.sub(r'[êé]', 'e', new_list[i])
+        new_list[i] = re.sub(r'[í]', 'i', new_list[i])
+        new_list[i] = re.sub(r'[ç]', 'c', new_list[i])
     
-    return new_string
+    return new_list
+
+
+def dict_converter(older_list: list) -> dict:
+    regex_key = r'([(]\w+[)])([\s\w,.()\-]+)'
+    new_dict = {}
+    for i in range(len(older_list)):
+        for k in re.finditer(regex_key, older_list[i], flags=re.I | re.M):
+            new_k_key = re.sub(r'[()]+', '', k.group(1))
+            new_k_value = k.group(2)
+            new_dict[new_k_key] = new_k_value[1:]
     
+    return new_dict
+            
 
 def get_objectives(URL: str):
     headers = {'user-agent': 'Mozilla/5.0'}
@@ -23,9 +37,8 @@ def get_objectives(URL: str):
     
     objectives = soup.find_all('div', attrs={'class': 'post-body entry-content'})
     filter_objective = re.findall(r'[(]\w+[)][\s\w,.()À-ú\-]+', str(objectives), flags=re.I | re.M)
-    filter_objective = sub_especial_characters(str(filter_objective))
-    
-    pprint(filter_objective)
+    filter_objective = sub_especial_characters(filter_objective)
+    filter_objective = dict_converter(filter_objective)
     
     with open('objectives.json', 'w') as file:
         json.dump(filter_objective, file)
